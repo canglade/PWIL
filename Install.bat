b3eaf558bdaff26ea2bffcd05f6062ca19162abb
Robin Bobo <robin.bobo@laposte.net>
2016-04-13 10:00:23 +0200

    Modification for package.json to include the proper dependencies

--- Install.bat
+++ Install.bat
@@ -1,14 +1,6 @@
 echo *** SERVER INSTALLATION ***
 
 call npm install -g npm
-call npm install angular
-call npm install express
-call npm install bower
-call npm install yo
-call npm install generator-angular
-call npm install grunt
-call npm install grunt-karma --save-dev
-call npm install mongoose 
 call npm install
 
 echo *** CLIENT INSTALLATION ***
